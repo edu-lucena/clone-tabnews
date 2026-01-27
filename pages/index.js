3886d95f726a86d67833b0e978fd863d14eb3f84
Eduardo Lucena <151290498+edu-lucena@users.noreply.github.com>
2026-01-27 00:43:20 +0000

    Mudança no texto da pagina

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 function Home() {
-  return <h1>Dak, eu amo você. Se você me ama, dá uma risadinha! </h1>;
+  const encoded = "aHR0cHM6Ly9jdXJzby5kZXYvd2ViL2Rucy1oNGNrM3I=";
+  const decoded = atob(encoded);
+
+  return (
+    <h1>
+      Eu amo sushi!!!
+      <br />
+      <a href={decoded}>{decoded}</a>
+    </h1>
+  );
 }
 
 export default Home;
