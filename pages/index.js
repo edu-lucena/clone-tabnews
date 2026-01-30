53b426506a31e0eb35b1bb89a78ebc404e2df2c4
Eduardo Lucena <151290498+edu-lucena@users.noreply.github.com>
2026-01-30 12:41:13 +0000

    Adiciona /api/v1/status endpoint

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,4 @@
 function Home() {
-  const encoded = "aHR0cHM6Ly9jdXJzby5kZXYvd2ViL2Rucy1oNGNrM3I=";
-  const decoded = atob(encoded);
-
-  return (
-    <h1>
-      Eu amo sushi!!!
-      <br />
-      <a href={decoded}>{decoded}</a>
-    </h1>
-  );
+  return <h1>Boraaaa!!</h1>;
 }
-
 export default Home;
